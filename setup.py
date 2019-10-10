08d02765e3f4c05ebddd065944534e07caed1758
Stephan Günther <gnn.code@gmail.com>
2019-10-10 16:05:18 +0200

    Drop support for Python versions < 3.5.3

--- setup.py
+++ setup.py
@@ -20,14 +20,12 @@ test_requirements = [ ]
 setup(
     author="Simon Hilpert",
     author_email='simon.hilpert@uni-flensburg.de',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=3.5.3',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
