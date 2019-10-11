2ff40f053bec884e20ca825587e4ef87b840e21f
simnh <simon.hilpert@uni-flensburg.de>
2019-10-11 12:18:31 +0200

    Add calculations module

---
+++ jopowa_vis/apps/calculations.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+from jopowa_vis.app import app
+
+
+def timeseries(data):
+    """ Calculates absolute timeseries based on scenario table entries and
+    normed profiles
+
+    Parameters
+    ----------
+    data : pd.Series
+        Series with scenario information on installed capacities and
+        energy (e.g. Demand.). Indices are supply technologies / demand.
+        Tame of series is the scenario.
+    """
+    ts = pd.DataFrame.from_dict(
+        {
+            tech: value * app.profiles[app.profile_mapper[tech]]
+            for tech, value in data.iteritems()
+            if tech in app.profile_mapper.keys()
+        }
+    ).set_index(app.profiles.index)
+    ts["Demand"] = ts["Demand"] * 1000
+    ts["RL"] = ts["Demand"] - ts[["Wind", "PV", "CSP"]].sum(axis=1)
+    ts["Other"] = ts["RL"][ts["RL"] > 0]
+    ts["Excess"] = ts["RL"][ts["RL"] < 0] * -1
+
+    ts.fillna(0, inplace=True)
+
+    return ts
