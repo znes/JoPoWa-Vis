604fa6249b3cbb80e3db7927f8351d3f75134bd9
Stephan Günther <gnn.code@gmail.com>
2019-10-11 11:30:43 +0200

    Blacken "setup.py"

--- setup.py
+++ setup.py
@@ -5,45 +5,45 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Simon Hilpert",
-    author_email='simon.hilpert@uni-flensburg.de',
-    python_requires='>=3.5.2',
+    author_email="simon.hilpert@uni-flensburg.de",
+    python_requires=">=3.5.2",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="JoPoWa-Vis",
     install_requires=requirements,
     license="BSD 3-Clause License",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='jopowa_vis',
-    name='jopowa_vis',
-    packages=find_packages(include=['jopowa_vis', 'jopowa_vis.*']),
-    py_modules=['jopowa_vis.app', 'jopowa_vis.jopowa_vis'],
+    keywords="jopowa_vis",
+    name="jopowa_vis",
+    packages=find_packages(include=["jopowa_vis", "jopowa_vis.*"]),
+    py_modules=["jopowa_vis.app", "jopowa_vis.jopowa_vis"],
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/znes/jopowa_vis',
-    version='0.0.1',
+    url="https://github.com/znes/jopowa_vis",
+    version="0.0.1",
     zip_safe=False,
 )
