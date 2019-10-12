abdd939c46bdd1bbd9c9009d5c2930a6b98b6ffc
simnh <simon.hilpert@uni-flensburg.de>
2019-10-12 16:56:21 +0200

    Make flake happy

--- jopowa_vis/apps/power_system.py
+++ jopowa_vis/apps/power_system.py
@@ -1,7 +1,7 @@
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 
 # import dash_table
 
